7aa789b2737c10d15dc329791a3052f4f3eab6c6
AndiShobash <andi500@gmail.com>
2023-07-04 13:01:12 +0300

    Service Discovery using Netflix Eureka

--- order-service/src/main/java/com/productservice/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/productservice/orderservice/service/OrderService.java
@@ -22,7 +22,7 @@ public class OrderService {
     @Autowired
     private OrderRepository orderRepository;
     @Autowired
-    private WebClient webClient;
+    private WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -41,8 +41,8 @@ public class OrderService {
                 .map(OrderLineItems::getSkuCode)
                 .toList();
         //Call Inventory Service, and place order if product is in stock
-        InventoryResponse[] inventoryResponsesArray = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponsesArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
