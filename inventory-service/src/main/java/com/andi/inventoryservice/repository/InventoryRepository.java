8b00bfa9e9ad534a3799cc20c3a27d28665c88d2
AndiShobash <andi500@gmail.com>
2023-07-04 10:39:35 +0300

    Inter Process Communication

--- inventory-service/src/main/java/com/andi/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/andi/inventoryservice/repository/InventoryRepository.java
@@ -3,8 +3,12 @@ package com.andi.inventoryservice.repository;
 import com.andi.inventoryservice.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
-    Optional<Inventory> findBySkuCode(String skuCode);
+    // This used at first when it was a single request
+    //Optional<Inventory> findBySkuCode(String skuCode);
+
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
 }
--- order-service/src/main/java/com/productservice/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/productservice/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.productservice.orderservice.service;
 
+import com.productservice.orderservice.dto.InventoryResponse;
 import com.productservice.orderservice.dto.OrderLineItemsDto;
 import com.productservice.orderservice.dto.OrderRequest;
 import com.productservice.orderservice.model.Order;
@@ -8,7 +9,9 @@ import com.productservice.orderservice.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -18,6 +21,9 @@ public class OrderService {
 
     @Autowired
     private OrderRepository orderRepository;
+    @Autowired
+    private WebClient webClient;
+
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -28,8 +34,29 @@ public class OrderService {
                 .toList();
 
         order.setOrderLineItemsList(orderLineItems);
+        System.out.println("Hi!");
 
-        orderRepository.save((order));
+        List<String> skuCodes = order.getOrderLineItemsList()
+                .stream()
+                .map(OrderLineItems::getSkuCode)
+                .toList();
+        //Call Inventory Service, and place order if product is in stock
+        InventoryResponse[] inventoryResponsesArray = webClient.get()
+                .uri("http://localhost:8082/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+
+        boolean allProductsInStock = Arrays.stream(inventoryResponsesArray)
+                .allMatch(InventoryResponse::isInStock);
+
+        if (allProductsInStock) {
+            orderRepository.save(order);
+        } else {
+            throw new IllegalArgumentException("Product is not is stock, please try again later");
+        }
+
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
